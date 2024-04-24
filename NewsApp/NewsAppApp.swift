db3f2730b8e0c93ea6a255b7c88ead6bfbad2c44
yusuftarikgun <yusuftarkgun@hotmail.com>
2024-04-24 10:18:16 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Yusuf Tarık Gün on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
